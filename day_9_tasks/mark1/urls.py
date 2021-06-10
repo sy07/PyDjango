54e966a33a29052e50412742776523cc904796b6
sy07 <soniyash0007@gmail.com>
2021-06-10 07:38:30 +0530

    tasks of day 9 @ akash technolabs

---
+++ day_9_tasks/mark1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+import os
+
+urlpatterns =[
+    path('',views.homepageview,name="home"),
+    path('about',views.aboutusview,name="aboutus"),
+    path('contact',views.contactview,name="contactus"),
+    path('myform',views.myform,name="myform"),
+    path('formprocess',views.process,name='process')
+]
